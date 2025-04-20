ac310facabb6de9286acd70b51c9a94a197159c9
Ilaha Algayeva <97362483+Ilahe2007@users.noreply.github.com>
2025-04-20 20:27:11 +0400

    Update snake.c

--- snake.c
+++ snake.c
@@ -1,150 +1,257 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <ncurses.h>
+#include <time.h>
+#include <stdlib.h>
 
-#define COL 24
-#define ROW 12
 #define DIG 100
 
-void clearGrid(char grid[ROW][COL]){
+void clearGrid(char grid[ROW][COL], int wall_places[ROW*COL], int wall_size){
 	// This function clears the grid. Fills every cell with whitespace
 	for(int i = 0; i < ROW; i++){
 		for(int j = 0; j < COL; j++){
 			if((i == 0) || (i == ROW - 1))
-				grid[i][j] = '-';
+				grid[i][j] = 'E';
+
 			else if((j == 0) || (j == COL - 1))
-				grid[i][j] = '|';
+				grid[i][j] = 'H';
+			
 			else
 				grid[i][j] = ' ';
 		}
+	}for(int i = 0; i < wall_size; i++){
+		grid[wall_places[i] / DIG][wall_places[i] % 100] = 'E';
 	}
 }
 
 void init_snake(int snake[], int *snake_size){
 	// This function initialize snake. It sets it body parts next to each other
-	*snake_size = 5;
+	*snake_size = 3;
 	for(int i = 0; i < *snake_size; i++){
-		snake[i] = 5 * DIG + (5 + i);
+		snake[*snake_size - i - 1] = (ROW / 2 - 1) * DIG + (1 + i);
+	}
+}
+
+void blank(char grid[ROW][COL], int blank_spaces[], int *size){
+	// This function get all the coordinates of blank spaces
+	for(int i = 0; i < ROW; i++){
+		for(int j = 0; j < COL; j++){
+			if(grid[i][j] == ' '){
+				blank_spaces[*size] = i * 100 + j;
+				(*size)++;
+			}
+		}
 	}
 }
 
 
-//checking to see if snake's head collides with walls' symbols or its body's symbols
-char collision_check(int snake[], int snake_size, char grid[ROW][COL]){
-	switch (grid[snake[snake_size - 1] / DIG][snake[snake_size - 1] % DIG]){
-		case '|':
+int random_blank_space(int blank_spaces[], int size, int snake_size){
+    // This function generates an coordinate of a blank space, 
+	if(snake_size >= (ROW - 1) * (COL - 1))
+		return -1;
+
+	int random_number = rand() % size;
+	return blank_spaces[random_number];
+}
+
+char collision_check(char grid[ROW][COL], int snake[]){
+	//checking to see if snake's head collides with walls' symbols or its body's symbols
+	switch (grid[snake[0] / DIG][snake[0] % DIG]){
+		case 'H': 
+		case 'E':
+			if(DIFFICULTY == 0)
+				return 0;
+		case '*':
 			return 2;
-		case '-':
-			return 2;
-		case '#':
-			return 2;
+		case '@':
+			return 1;
 	}
 
 	return 0;
 }
 
-void move_snake(int snake[], int *snake_size, char snake_facing){
-	int i, row, column;
+void move_snake(int snake[], int *snake_size, char snake_facing, int *grow){
+	// this function moves snake to the direction it is looking 
+	int row = snake[0] / DIG, column = snake[0] % DIG;
 	switch(snake_facing){
 		case '>':
-			row = snake[*snake_size - 1] / DIG;
-			column = snake[*snake_size - 1] % DIG + 1;
+			column++;
 			break;
 		case '^':
-			row = snake[*snake_size - 1] / DIG - 1;
-			column = snake[*snake_size - 1] % DIG;
+			row--;
 			break;
 		case '<':
-			row = snake[*snake_size - 1] / DIG;
-			column = snake[*snake_size - 1] % DIG - 1;
+			column--;
 			break;
 		case 'v':
-			row = snake[*snake_size - 1] / DIG + 1;
-			column = snake[*snake_size - 1] % DIG;
+			row++;
 			break;
 	}
-	for(i = 0; i < *snake_size - 1; i++){
-		snake[i] = snake[i + 1];
+	// if snake has eaten a food, increse its size
+	if(*grow){
+		*grow = 0;
+		(*snake_size)++;
 	}
-	snake[*snake_size - 1] = row * DIG + column;
+
+	// overwriting every element with one before.  
+	for(int i = *snake_size - 1; i > 0; i--){
+		snake[i] = snake[i - 1];
+	}
+
+	if(DIFFICULTY == 0){
+		if(row < 1)
+			row = ROW - 2;
+		else if(row > ROW - 2)
+			row = 1;
+		if(column < 1)
+			column = COL - 2;
+		else if(column > COL - 2)
+			column = 1;
+	}
+
+	// setting snake's head to its new location
+	snake[0] = row * DIG + column;
 }
 
+int coordinates(char grid[ROW][COL], int row, int col){
+	int c = 0;
+	if(grid[row + 1][col] == 'E' && (row + 1 != ROW - 1 && (col != COL - 1 && col != 0))){
+		c++;
+	}if(grid[row + 1][col + 1] == 'E' && (row + 1 != ROW - 1 && col + 1!= COL - 1)){
+		c++;
+	}if(grid[row + 1][col - 1] == 'E' && (row + 1 != ROW - 1 && col - 1 != 0)){
+		c++;
+	}if(grid[row - 1][col] == 'E' && (row - 1 != 0 && (col != COL - 1 && col != 0))){
+		c++;
+	}if(grid[row][col + 1] == 'E' && ((row != ROW - 1 && row != 0) && col + 1 != COL - 1)){
+		c++;
+	}if(grid[row][col - 1] == 'E' && ((row != ROW - 1 && row != 0) && col - 1 != 0)){
+		c++;
+	}if(grid[row - 1][col - 1] == 'E' && (row - 1 != 0 && col - 1 != 0)){
+		c++;
+	}if(grid[row - 1][col + 1] == 'E' && (row - 1 != 0 && col + 1 != COL - 1)){
+		c++;
+	}return c;
+}
 
+int game(void){
+	// initializing colors
+	init_pair(1, COLOR_RED,   -1);
+	init_pair(2, COLOR_YELLOW,-1);
+	init_pair(3, COLOR_GREEN, -1);
+	init_pair(4, COLOR_BLUE,  -1);
+	init_pair(5, -1,  -1);
 
-int main(void){
-	// Initializing ncurses for getting real time input
-	initscr();
-    cbreak();
-    noecho();
-    keypad(stdscr, TRUE);  
-    nodelay(stdscr, TRUE);
+	srand(time(NULL));
 
 	char grid[ROW][COL];
-	int snake[ROW * COL], snake_size;
+	int snake[ROW * COL], snake_size, food = 516, grow = 0, size = 0, wall = 0;
+	int blank_spaces[ROW * COL];
 	char snake_facing = '>';
-	int i, j;
-    char buf;
-	char run = 1;
+	int wall_places[ROW*COL], wall_size = 0;
 
-	char moved = '>', move_delay = 12, move_delay_index = 0;
-
-	clearGrid(grid);
+	clearGrid(grid, wall_places, wall_size);
 	init_snake(snake, &snake_size);
 
-    while (run){
-		buf = getch();
-        switch (buf){
-            case 'd':
-				if(moved != '<')
-					snake_facing = '>';
-                break;
-            case 'w':
-				if(moved != 'v')
-					snake_facing = '^';
-                break;
-            case 'a':
-				if(moved != '>')
-					snake_facing = '<';
-                break;
-            case 's':
-				if(moved != '^')
-					snake_facing = 'v';
-                break;
-            case 'q':
-                run = 0;
+	blank(grid, blank_spaces, &size);
+
+	char run = 1, moved = '>', move_index = 0;
+    int times_snake_moves = 0;
+	while (run){
+		switch (getch()){
+		case 'd': case KEY_RIGHT:
+			if(moved != '<')
+				snake_facing = '>';
+            break;
+		case 'w':case KEY_UP:
+			if(moved != 'v')
+				snake_facing = '^';
+            break;
+        case 'a':case KEY_LEFT:
+			if(moved != '>')
+				snake_facing = '<';
+        	break;
+        case 's': case KEY_DOWN:
+		    if(moved != '^')
+			    snake_facing = 'v';
+			break;
+        case 'q':
+            run = 0;
         }
-		clearGrid(grid);
-		erase();
+    clearGrid(grid, wall_places, wall_size);
+    erase();
 
-		if(move_delay < move_delay_index){
-			move_snake(snake, &snake_size, snake_facing);
-			move_delay_index = 0;
-			moved = snake_facing;
-		}
+    grid[food / DIG][food % DIG] = '@';
+    grid[wall / DIG][wall % DIG] = 'E';
+    
 
-		for(i = 0; i < snake_size - 1; i++){
-			grid[snake[i] / DIG][snake[i] % DIG] = '#';
-		} 
-		//Check the head before overwriting.
-		if (collision_check(snake,snake_size,grid) == 2){
+	if(SPEED < move_index){
+		move_snake(snake, &snake_size, snake_facing, &grow);
+		move_index = 0;
+		moved = snake_facing;
+	}
+
+	for(int i = snake_size - 1; i > 0; i--){
+		grid[snake[i] / DIG][snake[i] % DIG] = '*';
+	} 
+
+	//Check the head before overwriting.
+	switch(collision_check(grid, snake)){
+		case 2:
 			run = 0;
-		}
-
-		grid[snake[snake_size - 1] / DIG][snake[snake_size - 1] % DIG] = '@';
-
-		for(i = 0; i < ROW; i++){
-			for(j = 0; j < COL; j++){
-				printw("%c", grid[i][j]);
+			break;
+		case 1:
+            size = 0;
+            blank(grid, blank_spaces, &size);
+            food = random_blank_space(blank_spaces, size, snake_size);
+			
+			grid[food / DIG][food % DIG] = '@';
+			if(coordinates(grid, food / 100, food % 100) < 3){
+				size = 0;
+				if(DIFFICULTY == 2){
+					blank(grid, blank_spaces, &size);
+					wall = random_blank_space(blank_spaces, size, snake_size);
+					wall_places[wall_size] = wall;
+					wall_size++;
+				}grow = 1;
 			}
-			printw("\n");
-		}
+	}
 
-		refresh();
-		move_delay_index++;
-		usleep(16000);
+	grid[snake[0] / DIG][snake[0] % DIG] = 'O';
+
+	for(int i = 0; i < ROW; i++){
+		for(int j = 0; j < COL; j++){
+			switch(grid[i][j]){
+				case '@':
+					attron(COLOR_PAIR(1));
+					break;
+				case 'O':
+					attron(COLOR_PAIR(2));
+					break;
+				case '*':
+					attron(COLOR_PAIR(3));
+					break;
+				default:
+					attron(COLOR_PAIR(4));
+			}
+			printw("%c ", grid[i][j]);
+		}
+		if(i == (ROW / 2 - 1)){
+			attron(COLOR_PAIR(5));
+			printw("\tYour score is: %d", (snake_size - 3));
+		}
+		printw("\n");
+	}
+
+	refresh();
+	move_index++;
+	usleep(16000);
 
     }
-    
-	endwin();
+	attron(COLOR_PAIR(5));
+    erase();
+	printw("\n\n\t\tSkill Issue: %d", snake_size - 3);
+	refresh();
+	usleep(2000000);
     return 0;
 }
